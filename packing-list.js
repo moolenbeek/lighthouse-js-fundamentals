b816bdcc2a10b756c0bbf83b51a14e6c532301bb
Eric Moolenbeek <emoolenbeek@gmail.com>
2021-11-05 17:41:16 +0000

    added while loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,15 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+// display array items using for loop
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+// display array items using while loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
